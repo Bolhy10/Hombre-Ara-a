dbfe93e444fa0cf73c5da2e89aef5d156c750fb0
Bolhy10 <bolhy10@hotmail.com>
2014-11-06 04:12:25 -0500

    Prueba del manejo de Git

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Prueba de funcionamiento</title></head>
+<body>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bolhy
+ * Date: 06/11/2014
+ * Time: 4:05
+ */
+echo "<p>Hola Mundo como esta</p>";
+?>
+</body>
+</html>
